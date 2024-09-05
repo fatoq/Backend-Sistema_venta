59c18ddb4f53a89ae08e56c0e042329131711cf0
fatoq <fausto_ronaldo@hotmail.com>
2024-09-05 15:38:53 -0500

    Elaboracion del esquema de productos

---
+++ models/products.js
@@ -0,0 +1,15 @@
+'use strict'
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var ProductSchema = Schema({
+    codigoBarra: { type: String, required: true, unique: true },
+    nombre: { type: String, required: true },
+    categoria: { type: String, required: true },
+    stock: { type: Number, required: true },
+    precio: { type: Number, required: true },
+    disponible: { type: Boolean, default: true },
+    vendido: { type: Number, default: 0 },
+});
+
+module.exports = mongoose.model('Product', ProductSchema);
