9fa2cc3431c6164b1d6fc8e1c0216fbd232ae478
fatoq <fausto_ronaldo@hotmail.com>
2024-10-11 18:20:50 -0500

    se mejoro la nota de venta

--- controllers/notaVenta.controller.js
+++ controllers/notaVenta.controller.js
@@ -1,7 +1,7 @@
 'use strict'
 var NotaVenta = require('../models/cliente');
 var Venta=require('../models/ventas')
-const PDFDocument = require('pdfkit');
+const PDFDocument = require('pdfkit-table');
 const { body, validationResult } = require('express-validator');
 const fs = require('fs');
 const path = require('path');
@@ -47,10 +47,8 @@ var controller = {
     genenNotaventa: async function(req, res) {
         const { notaVentaId } = req.params;
         const directoryPath = path.join(__dirname, '../notas_venta_pdf');
-        //const filePath = path.join(directoryPath, `Nota_venta_${notaVentaId}.pdf`);
         const file=path.join(directoryPath,`Nota_venta_${notaVentaId}.pdf`);
-        try {
-            // Verifica si la carpeta 'notas_venta_pdf' existe, si no, la crea
+        try {             // si la carpeta 'notas_venta_pdf' existe, sino la crea
             if (!fs.existsSync(directoryPath)) {
                 fs.mkdirSync(directoryPath, { recursive: true });
             }
@@ -75,30 +73,85 @@ var controller = {
             const doc = new PDFDocument();
             const pdfStream = fs.createWriteStream(file);          
             doc.pipe(pdfStream);
-            doc.fontSize(18).text('Nota de Venta', { align: 'center' });
+            //logo
+            const imagePath = path.join(__dirname, '../notas_venta_pdf/logo.png');  // Ajustar la ruta relativa
+            // Verificar si la imagen existe
+            if (fs.existsSync(imagePath)) {
+                // Insertar la imagen en el lado izquierdo del título "NOTA DE VENTA"
+                doc.image(imagePath, 50, 5, { width: 130, height: 100 });
+            } else {
+                console.log('El archivo de imagen no se encontró:', imagePath);
+            }
+            //Encabezado
+            doc.fontSize(32).font('Helvetica-Bold').text('NOTA DE VENTA', { align: 'right' });
+            doc.fontSize(32).font('Helvetica-Bold').text('Viveres Ronaldo', { align: 'center' });
+            doc.fontSize(11).text('Leonidas Plaza-272 y Lizardo Garcia N24; Cel:0998866499', { align: 'center' });
+            doc.moveTo(50, 100).lineTo(550, 100).stroke();
             doc.moveDown();
-            doc.fontSize(12).text(`Cliente: ${notaVenta.cliente.nombre}`);
-            doc.text(`C.I.: ${notaVenta.cliente.ci}`);
-            doc.text(`Teléfono: ${notaVenta.cliente.telefono}`);
-            doc.text(`Dirección: ${notaVenta.cliente.direccion}`);
+            doc.fontSize(10.5).text('Atendiendo a su amable solicitud se emite la correspondiente nota de venta', { align: 'center' });
             doc.moveDown();
-            doc.text(`Fecha: ${notaVenta.fecha.toLocaleDateString('es-ES', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' })}`);
+            // Datos del cliente y venta
+            doc.fontSize(12).font('Helvetica-Bold').text('Cliente:', 50, doc.y);  // Usar doc.y para obtener la posición actual y colocar elementos después
+            doc.moveTo(150, doc.y -2).lineTo(550, doc.y - 2).stroke();  // Línea subrayada para "Cliente"
+            doc.fontSize(12).font('Helvetica').text(notaVenta.cliente.nombre, 315, doc.y-18); 
             doc.moveDown();
-            // productos
-            notaVenta.venta.productos.forEach((item) => {
-                const producto = item.producto;  
-                const cantidad = item.cantidad;
-                const precio = producto.precio;  
-                const nombre = producto.nombre;
-                doc.text(`Producto: ${nombre} - Cantidad: ${cantidad} - Precio: ${precio} - Total: ${cantidad * precio}`);
+            // Espacio y línea para "C.I./R.U.C"
+            doc.fontSize(12).font('Helvetica-Bold').text('C.I./R.U.C:', 50, doc.y);
+            doc.moveTo(150, doc.y -2).lineTo(550, doc.y -2).stroke();
+            doc.fontSize(12).font('Helvetica').text(notaVenta.cliente.ci, 315, doc.y-18);
+            doc.moveDown();
+            // Espacio y línea para "Teléfono"
+            doc.fontSize(12).font('Helvetica-Bold').text('Teléfono:', 50, doc.y);
+            doc.moveTo(150, doc.y -2).lineTo(550, doc.y -2).stroke();
+            doc.fontSize(12).font('Helvetica').text(notaVenta.cliente.telefono, 315, doc.y-18);
+            doc.moveDown();
+            // Espacio y línea para "Dirección"
+            doc.fontSize(12).font('Helvetica-Bold').text('Dirección:', 50, doc.y);
+            doc.moveTo(150, doc.y -2).lineTo(550, doc.y -2).stroke();
+            doc.fontSize(12).font('Helvetica').text(notaVenta.cliente.direccion, 315, doc.y-18);
+            doc.moveDown();
+            // Espacio y línea para "Fecha"
+            doc.fontSize(12).font('Helvetica-Bold').text('Fecha:', 50, doc.y);
+            doc.moveTo(150, doc.y -2).lineTo(550, doc.y -2).stroke();
+            doc.fontSize(12).font('Helvetica').text(notaVenta.fecha.toLocaleDateString('es-ES', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' }), 281, doc.y-18);
+            doc.moveDown();
+            doc.moveDown();
+            // productos de la venta
+            const productos = notaVenta.venta.productos.map(item => ({
+                cantidad: item.cantidad,
+                descripcion: item.producto.nombre,
+                precioUnitario: item.producto.precio,
+                precioTotal: item.cantidad * item.producto.precio
+            }));
+            // Crear tabla con los productos
+            const table = {
+                title: 'Detalles de la venta',
+                headers: ['Cantidad', 'Descripción', 'Precio Unitario', 'Precio Total'],
+                rows: productos.map(item => [
+                    item.cantidad,
+                    item.descripcion,
+                    `$${item.precioUnitario.toFixed(2)}`,
+                    `$${item.precioTotal.toFixed(2)}`
+                ])
+            };
+            // Insertar la tabla en el PDF justo después de los datos del cliente
+            doc.table(table, {
+                prepareHeader: () => doc.font('Helvetica-Bold').fontSize(10),
+                prepareRow: (row, i) => doc.font('Helvetica').fontSize(10),
+                width: 500,
+                columnSpacing: 10,
+                x: 50,  // Posición X de la tabla (mantener fija)
             });
-            doc.text(`Total: ${notaVenta.venta.total}`);
+            // Total de la venta
+            doc.moveDown();
+            doc.fontSize(12).font('Helvetica-Bold').text(`Total de la Venta: $${notaVenta.venta.total.toFixed(2)}`, { align: 'right' });
+            doc.moveDown();
+            doc.fontSize(17).font('Helvetica-Bold').text(`Gracias por preferirnos`, { align: 'center' });
+            // Finalizar el documento
             doc.end();
             pdfStream.on('finish',function(){
                 console.log('PDF generado correctamente');
-               //res.status(200).send({ message: 'PDF generado correctamente' });
                 res.download(file, `Nota_venta_${notaVentaId}.pdf`);
-
             });
             pdfStream.on('error',function(err){
                 console.error('Error generando PDF:', err);
